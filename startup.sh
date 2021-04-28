465f0c402135441fed7b2557ebd9225a07c63f43
Jioh L. Jung <ziozzang@gmail.com>
2021-04-28 19:57:37 +0900

    fix scripts

--- startup.sh
+++ startup.sh
@@ -2,42 +2,21 @@
 CLAMAV_MIRROR=${CLAMAV_MIRROR:-"http://firewall.host:8000"}
 CLAMAV_PORT=${CLAMAV_PORT:-"3310"}
 
-if [ -z "${CLAMAV_MIRROR}" ]; then
-
-	echo ">>> CLAMAV_MIRROR: ${CLAMAV_MIRROR}"
-cat > /etc/clamav/freshclam.conf <<EOF
-DatabaseOwner clamav
-UpdateLogFile /var/log/clamav/freshclam.log
-LogVerbose false
-LogSyslog false
-LogFacility LOG_LOCAL6
-LogFileMaxSize 0
-LogRotate true
-LogTime true
-Foreground false
-Debug false
-MaxAttempts 5
-DatabaseDirectory /var/lib/clamav
-#DNSDatabaseInfo current.cvd.clamav.net
-ConnectTimeout 30
-ReceiveTimeout 0
-TestDatabases yes
-ScriptedUpdates yes
-CompressLocalDatabase no
-SafeBrowsing false
-Bytecode true
-NotifyClamd /etc/clamav/clamd.conf
-# Check for new database 24 times a day
-Checks 24
-DatabaseMirror ${CLAMAV_MIRROR}
-EOF
+if [ ! -z "${CLAMAV_MIRROR}" ]; then
+	echo ">>> Setup CLAMAV_MIRROR: ${CLAMAV_MIRROR}"
+	sed -i 's,^\(DatabaseMirror\),#\1,' /etc/clamav/freshclam.conf
+	sed -i 's,^\(DNSDatabaseInfo\),#\1,' /etc/clamav/freshclam.conf
+	echo "DatabaseMirror ${CLAMAV_MIRROR}" >> /etc/clamav/freshclam.conf
 fi
 
 echo "TCPSocket ${CLAMAV_PORT}" >> /etc/clamav/clamd.conf
 
 chown -R clamav:clamav /var/run/clamav
 
-#freshclam
+# Initial downloading
+freshclam
+
+# Daemon Starting
 freshclam -d
 
 clamd -F
